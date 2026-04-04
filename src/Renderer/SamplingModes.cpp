ff302741e612e706fd0b2dc491246a6d600fe9ce
Zeshan Rasul <zeshanrasul90@gmail.com>
2026-04-04 16:43:33 +0100

    Increase action space slightly to be less coarse

--- src/Renderer/SamplingModes.cpp
+++ src/Renderer/SamplingModes.cpp
@@ -4,9 +4,11 @@ SamplingModeParams GetSamplingModeParams(SamplingMode mode)
 {
     switch (mode)
     {
-    case SamplingMode::BSDFHeavy:  return { 0.9f, 0.35f };
-    case SamplingMode::Balanced:   return { 0.5f, 0.75f };
-    case SamplingMode::LightHeavy: return { 0.1f, 1.0f };
-    default:                       return { 0.5f, 0.75f };
+    case SamplingMode::BSDFHeavy:  return { 0.9f, 0.1f };
+    case SamplingMode::BSDFGentle:  return { 0.7f, 0.3f };
+    case SamplingMode::Balanced:   return { 0.5f, 0.5f };
+    case SamplingMode::LightGentle: return { 0.3f, 0.7f };
+    case SamplingMode::LightHeavy: return { 0.1f, 0.9f };
+    default:                       return { 0.5f, 0.5f };
     }
 }
